53d3e48971c5855f0812a20bba2c431571f1f2f8
MikeFranco <mfranco_98@yahoo.com>
2020-04-23 18:06:42 -0500

    changed head

--- .github/actions/debug-action/__tests__/debug.test.ts
+++ .github/actions/debug-action/__tests__/debug.test.ts
@@ -18,7 +18,7 @@ beforeEach(() => {
     },
   } as WebhookPayload;
 
-  console.log('%c⧭', 'color: #f20606', 'para el test 3');
+  console.log('%c⧭', 'color: #f20606', 'para el test 2');
 });
 
 afterEach(() => {
