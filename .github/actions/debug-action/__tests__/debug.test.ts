3d217788f254d2485e48a49b952a0f2320f0047e
MikeFranco <mfranco_98@yahoo.com>
2020-04-23 17:23:22 -0500

    changed color in check tests workflow

--- .github/actions/debug-action/__tests__/debug.test.ts
+++ .github/actions/debug-action/__tests__/debug.test.ts
@@ -18,7 +18,7 @@ beforeEach(() => {
     },
   } as WebhookPayload;
 
-  console.log('%c⧭', 'color: #f2ceb6', 'para el test');
+  console.log('%c⧭', 'color: #f20606', 'para el test');
 });
 
 afterEach(() => {
