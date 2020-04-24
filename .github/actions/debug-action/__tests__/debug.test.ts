1f4d2de9c2b39cba28c0d9f239f8d1c31cb902f8
MikeFranco <mfranco_98@yahoo.com>
2020-04-24 15:33:10 -0500

    changed Token

--- .github/actions/debug-action/__tests__/debug.test.ts
+++ .github/actions/debug-action/__tests__/debug.test.ts
@@ -18,7 +18,7 @@ beforeEach(() => {
     },
   } as WebhookPayload;
 
-  console.log('%c⧭', 'color: #f20606', 'para el test 0');
+  console.log('%c⧭', 'color: #f20606', 'para el test 1');
 });
 
 afterEach(() => {
