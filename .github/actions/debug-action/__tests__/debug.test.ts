a67562f8d002c762d72b06fb1c95b77e8703fa3b
MikeFranco <mfranco_98@yahoo.com>
2020-04-23 19:10:33 -0500

    multline run added

--- .github/actions/debug-action/__tests__/debug.test.ts
+++ .github/actions/debug-action/__tests__/debug.test.ts
@@ -18,7 +18,7 @@ beforeEach(() => {
     },
   } as WebhookPayload;
 
-  console.log('%c⧭', 'color: #f20606', 'para el test 2');
+  console.log('%c⧭', 'color: #f20606', 'para el test 1000');
 });
 
 afterEach(() => {
